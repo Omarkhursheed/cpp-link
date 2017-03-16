ed645d352a37d9a3ff46e10e70e04caf329e5811
omarkhursheed <omarkhu@gmail.com>
2017-03-16 22:23:19 +0530

    added prime number generator

---
+++ primes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime (int n)
+{
+  if (n<=1) return false;
+  if (n==2) return true;
+  if (n%2==0) return false;
+  int m  = sqrt(n);
+
+  for (int i = 3; i <= m; i+=2)
+  {
+    if (n%i==0)
+      return false;
+  }
+  return true;
+}
+
+int main(int argc, char const *argv[])
+{
+  cout << isPrime(30) << endl;
+  return 0;
+}
